550c1aa6dcef3e69773199e47adb3c909380e6ff
Omar Mirza <omar.creative.dev@gmail.com>
2019-03-09 10:59:00 +0000

    updates to header, footer and sass files

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faCode, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { faGooglePlusG, faLinkedinIn, faSkype, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -11,6 +12,10 @@ export class FooterComponent {
 
   public faCode: IconDefinition = faCode;
   public faArrowUp: IconDefinition = faArrowUp;
+  public faSkype: IconDefinition = faSkype;
+  public faLinkedinIn: IconDefinition = faLinkedinIn;
+  public faTwitter: any = faTwitter;
+  public faGooglePlusG: IconDefinition = faGooglePlusG;
 
   public currentYear(): number {
     return new Date().getFullYear();
