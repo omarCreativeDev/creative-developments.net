3b1efafa3b676d039c6611f3ab1fa1b3e1fbbbf2
Omar Mirza <omar.creative.dev@gmail.com>
2019-03-29 09:25:06 +0000

    Added travis deploy script to automate deployments to production

---
+++ travis-deploy-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+printf 'Deploy stage started...' >&2
+
+# Create zip package
+tar -czf tar-package.tgz dist
+
+# Transfer zip package to remote server via scp
+sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no tar-package.tgz "$REMOTE_USER"@"$REMOTE_IP:$REMOTE_PATH"
+
+# sleep 1 min to ensure scp package transfer completes
+sleep 1m
+
+# Connect to remote server via ssh
+sshpass -p "$REMOTE_PASS" ssh "$REMOTE_USER"@"$REMOTE_IP" "cd $REMOTE_PATH; ./deploy.sh"
