c10afe291a7937605a5fc4319c7970671145b953
Omar Mirza <omar.creative.dev@gmail.com>
2019-09-12 08:45:53 +0100

    calculate years of experience and remove redundant scss file

--- src/app/features/home/home-page/home-page.component.ts
+++ src/app/features/home/home-page/home-page.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Moment } from 'moment';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class HomePageComponent {
+  private beganDevelopment: Moment = moment('01/03/2008', 'MM-DD-YYYY');
+  public yearsOfExperience: number = moment().diff(this.beganDevelopment, 'years');
 }
