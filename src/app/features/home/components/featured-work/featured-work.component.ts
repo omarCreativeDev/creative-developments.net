12ac2e66c6a284b3cf71f9b48fa89398c50aa0e1
Omar Mirza <omar.creative.dev@gmail.com>
2019-10-15 11:07:35 +0100

    created interface and looped over featured work items in component

--- src/app/features/home/components/featured-work/featured-work.component.ts
+++ src/app/features/home/components/featured-work/featured-work.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IShowcaseItem } from '@core/model/interfaces';
 
 @Component({
   selector: 'app-featured-work',
   templateUrl: './featured-work.component.html',
   styleUrls: ['./featured-work.component.scss'],
 })
-export class FeaturedWorkComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class FeaturedWorkComponent {
+  public featuredWork: IShowcaseItem[] = [
+    {
+      caption: 'Featured Work | Interoute | Pricing',
+      imageName: 'interoute-pricing',
+    },
+    {
+      caption: 'Featured Work | Interoute | Basket',
+      imageName: 'interoute-basket',
+    },
+    {
+      caption: 'Featured Work | Interoute | Quick Deployment',
+      imageName: 'interoute-quick-deployment',
+    },
+  ];
 }
