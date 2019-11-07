ca45ad189bcbf3715a7b9c17d38342441a1aa081
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-07 17:00:24 +0000

    updated avatar-and-hobbies component

--- src/app/features/about-me/components/avatar-and-hobbies/avatar-and-hobbies.component.ts
+++ src/app/features/about-me/components/avatar-and-hobbies/avatar-and-hobbies.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-avatar-and-hobbies',
   templateUrl: './avatar-and-hobbies.component.html',
   styleUrls: ['./avatar-and-hobbies.component.scss'],
 })
-export class AvatarAndHobbiesComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class AvatarAndHobbiesComponent {}
--- src/app/features/about-me/components/more-info/more-info.component.ts
+++ src/app/features/about-me/components/more-info/more-info.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-more-info',
   templateUrl: './more-info.component.html',
-  styleUrls: ['./more-info.component.scss'],
 })
-export class MoreInfoComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class MoreInfoComponent {}
