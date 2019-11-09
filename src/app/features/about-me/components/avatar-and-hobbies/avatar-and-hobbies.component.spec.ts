1508d6f0275fc56919469aff11cb074d19a0a8c7
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-09 10:11:48 +0000

    markup tweak and fix unit test

--- src/app/features/about-me/components/avatar-and-hobbies/avatar-and-hobbies.component.spec.ts
+++ src/app/features/about-me/components/avatar-and-hobbies/avatar-and-hobbies.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AvatarAndHobbiesComponent } from './avatar-and-hobbies.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AvatarAndHobbiesComponent', () => {
   let component: AvatarAndHobbiesComponent;
@@ -9,6 +10,7 @@ describe('AvatarAndHobbiesComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AvatarAndHobbiesComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
