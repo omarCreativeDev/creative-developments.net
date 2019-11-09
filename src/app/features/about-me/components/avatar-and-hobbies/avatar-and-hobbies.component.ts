5961f51be70f127854767e5f555ac0ba98386e38
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-09 09:42:33 +0000

    fix layout issues

--- src/app/features/about-me/components/avatar-and-hobbies/avatar-and-hobbies.component.ts
+++ src/app/features/about-me/components/avatar-and-hobbies/avatar-and-hobbies.component.ts
@@ -14,6 +14,7 @@ export class AvatarAndHobbiesComponent {
     },
     chart: {
       height: 200,
+      width: 200,
       renderTo: 'hobbiesHighChart',
       plotBackgroundColor: null,
       plotBorderWidth: null,
