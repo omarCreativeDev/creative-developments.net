0c7c1a8cb6a8e4220f1802934741a4d90e6a88e9
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-19 15:08:00 +0000

    finished adding skill groups

--- src/app/features/my-skills/components/my-skills-summary/my-skills-summary.component.ts
+++ src/app/features/my-skills/components/my-skills-summary/my-skills-summary.component.ts
@@ -28,5 +28,12 @@ export class MySkillsSummaryComponent {
       ],
     },
     { label: 'Unit testing', skills: ['Jasmine', 'Karma', 'Mocha', 'Chai'] },
+    { label: 'E2E testing', skills: ['Protractor', 'Gherkin', 'Selenium', 'Webdriver IO'] },
+    { label: 'Data Visualisation', skills: ['Highcharts', 'Ng2 charts'] },
+    { label: 'REST', skills: ['RESTful API', 'Swagger', 'Postman', 'Mocky'] },
+    { label: 'Design', skills: ['Photoshop', 'Sketch', 'Invision'] },
+    { label: 'State management', skills: ['NGRX', 'NGXS'] },
+    { label: 'Code reviews', skills: ['Pull requests', 'Pair programming', 'Crucible'] },
+    { label: 'Devops', skills: ['Travis', 'Jenkins', 'Amazon web services', 'Docker'] },
   ];
 }
