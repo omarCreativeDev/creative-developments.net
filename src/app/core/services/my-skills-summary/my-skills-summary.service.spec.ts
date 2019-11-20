d285e83cbd3b91bb0ace823cf603858b69ac805f
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-20 06:44:31 +0000

    moved common data out to service

---
+++ src/app/core/services/my-skills-summary/my-skills-summary.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MySkillsSummaryService } from './my-skills-summary.service';
+
+describe('MySkillsSummaryService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: MySkillsSummaryService = TestBed.get(MySkillsSummaryService);
+    expect(service).toBeTruthy();
+  });
+});
--- src/app/features/my-skills/components/my-skills-summary/my-skills-summary.component.ts
+++ src/app/features/my-skills/components/my-skills-summary/my-skills-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ISkillsSummaryGroup } from '@core/model/interfaces';
+import { MySkillsSummaryService } from '@core/services/my-skills-summary/my-skills-summary.service';
 
 @Component({
   selector: 'app-my-skills-summary',
@@ -7,47 +8,16 @@ import { ISkillsSummaryGroup } from '@core/model/interfaces';
   styleUrls: ['./my-skills-summary.component.scss'],
 })
 export class MySkillsSummaryComponent {
-  public groups: ISkillsSummaryGroup[] = [
-    {
-      label: 'UI Stack',
-      active: false,
-      skills: ['HTML5', 'CSS3', 'CSS Grid', 'Responsive design', 'SASS / LESS', 'Flexbox', 'BEM'],
-    },
-    {
-      label: 'UI Libraries',
-      active: false,
-      skills: ['Angular material', 'Bootstrap', 'GOV.UK design system (GDS)', 'Clarity UI'],
-    },
-    {
-      label: 'Javascript stack',
-      active: true,
-      skills: [
-        'Javascript',
-        'ES6',
-        'Typescript',
-        'RxJs',
-        'Angular',
-        'Webpack',
-        'RESTful API’s',
-        'Node',
-        'NPM / Yarn',
-        'Moment',
-        'Lo dash',
-        'React',
-      ],
-    },
-    { label: 'Unit testing', active: false, skills: ['Jasmine', 'Karma', 'Mocha', 'Chai'] },
-    { label: 'E2E testing', active: false, skills: ['Protractor', 'Gherkin', 'Selenium', 'Webdriver IO'] },
-    { label: 'Data Visualisation', active: false, skills: ['Highcharts', 'Ng2 charts'] },
-    { label: 'REST', active: false, skills: ['RESTful API', 'Swagger', 'Postman', 'Mocky'] },
-    { label: 'Design', active: false, skills: ['Photoshop', 'Sketch', 'Invision'] },
-    { label: 'State management', active: false, skills: ['NGRX', 'NGXS'] },
-    { label: 'Code reviews', active: false, skills: ['Pull requests', 'Pair programming', 'Crucible'] },
-    { label: 'Devops', active: false, skills: ['Travis', 'Jenkins', 'Amazon web services', 'Docker'] },
-  ];
+  public groups: ISkillsSummaryGroup[] = this.mySkillsSummaryService.groups;
 
-  public toggle(group: ISkillsSummaryGroup): void {
+  /**
+   * Toggle group skill details visibility
+   * @param group ISkillsSummaryGroup
+   */
+  public toggleVisibility(group: ISkillsSummaryGroup): void {
     this.groups.map(item => (item.active = false));
     group.active = !group.active;
   }
+
+  constructor(private mySkillsSummaryService: MySkillsSummaryService) {}
 }
