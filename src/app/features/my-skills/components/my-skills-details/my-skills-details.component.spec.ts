253db0eecb322d1744e9fb646a16900306d4accb
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-20 10:30:47 +0000

    fix unit tests

--- src/app/features/my-skills/components/my-skills-details/my-skills-details.component.spec.ts
+++ src/app/features/my-skills/components/my-skills-details/my-skills-details.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MySkillsDetailsComponent } from './my-skills-details.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('MySkillsDetailsComponent', () => {
   let component: MySkillsDetailsComponent;
@@ -9,6 +10,7 @@ describe('MySkillsDetailsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [MySkillsDetailsComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
