50367e6723cabf43d5a6525c45131f63033fcbe5
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-22 14:26:29 +0000

    styled contact details

--- src/app/features/contact-me/pages/contact-me-page/contact-me-page.component.spec.ts
+++ src/app/features/contact-me/pages/contact-me-page/contact-me-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactMePageComponent } from './contact-me-page.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ContactMePageComponent', () => {
   let component: ContactMePageComponent;
@@ -9,6 +10,7 @@ describe('ContactMePageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ContactMePageComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
