72108a3bbe17f2ad0f3937dafc5fa15c59b7ae49
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-23 16:59:10 +0000

    fix unit tests

--- src/app/features/contact-me/components/contact-form/contact-form.component.spec.ts
+++ src/app/features/contact-me/components/contact-form/contact-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactFormComponent } from './contact-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ContactFormComponent', () => {
   let component: ContactFormComponent;
@@ -9,6 +10,7 @@ describe('ContactFormComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ContactFormComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   }));
 
