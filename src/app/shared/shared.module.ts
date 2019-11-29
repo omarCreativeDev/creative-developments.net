7019a1d2e41c2901b9afb6ff9760c158529d40c1
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-29 07:59:49 +0000

    added httpClient to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { YearsOfExperienceComponent } from './components/years-of-experience/years-of-experience.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, YearsOfExperienceComponent],
-  imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule, RouterModule, HttpClientModule],
   exports: [HeaderComponent, FooterComponent, FontAwesomeModule, YearsOfExperienceComponent],
 })
 export class SharedModule {}
