21283fd27ca43a94b368a7e235e3269c38e24202
Omar Mirza <omar.creative.dev@gmail.com>
2019-11-29 10:57:58 +0000

    fixed broken unit tests

--- src/app/core/services/email/email.service.spec.ts
+++ src/app/core/services/email/email.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
 import { EmailService } from './email.service';
+export class EmailServiceMock {}
 
 describe('EmailService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [{ provide: EmailService, useClass: EmailServiceMock }],
+    })
+  );
 
   it('should be created', () => {
     const service: EmailService = TestBed.get(EmailService);
--- src/app/features/contact-me/components/contact-form/contact-form.component.spec.ts
+++ src/app/features/contact-me/components/contact-form/contact-form.component.spec.ts
@@ -2,6 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactFormComponent } from './contact-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EmailService } from '@core/services/email/email.service';
+
+export class EmailServiceMock {}
 
 describe('ContactFormComponent', () => {
   let component: ContactFormComponent;
@@ -11,6 +14,7 @@ describe('ContactFormComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ContactFormComponent],
       imports: [ReactiveFormsModule],
+      providers: [{ provide: EmailService, useClass: EmailServiceMock }],
     }).compileComponents();
   }));
 
