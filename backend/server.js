4c8038d372ec92fa55b4316d87b5caf1e5c960d7
Omar Mirza <omar.creative.dev@gmail.com>
2021-12-08 08:16:54 +0000

    added error log

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
 app.listen(3000, () => {
-  console.log('server started on port 3000');
+  console.log("server started on port 3000");
 });
 
-app.post('/sendEmail', (req, res) => {
-  console.log('request received');
+app.post("/sendEmail", (req, res) => {
   let user = req.body;
-  sendMail(user, info => {
+  sendMail(user, (info) => {
     console.log(`Email sent!`, `Id: ${info.messageId}`);
     res.send(info);
-  });
+  }).catch((error) => console.log("error", error));
 });
 
 async function sendMail(data, callback) {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'info.creativedevelopments@gmail.com',
-      pass: 'ihatemanu123',
-    },
+      user: "info.creativedevelopments@gmail.com",
+      pass: "ihatemanu123"
+    }
   });
 
   let mailOptions = {
     from: `${data.name} <${data.email}>`, // sender address
-    to: 'omar.creative.dev@gmail.com', // list of receivers
+    to: "omar.creative.dev@gmail.com", // list of receivers
     subject: `Creative Developments enquiry from ${data.name}`, // Subject line
     html: `<p><b>Name:</b> ${data.name}</p>
     <p><b>Phone:</b> ${data.phone}</p>
     <p><b>Email:</b> ${data.email}</p>
-    <p><b>Message:</b> ${data.message}</p>`,
+    <p><b>Message:</b> ${data.message}</p>`
   };
 
   // send mail with defined transport object
