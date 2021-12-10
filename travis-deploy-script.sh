c9fe3b04a86fe7b3fa6fe4e20bfb273c79cebe23
Omar Mirza <omar.creative.dev@gmail.com>
2021-12-10 09:44:30 +0000

    cleanup

--- travis-deploy-script.sh
+++ travis-deploy-script.sh
@@ -13,32 +13,27 @@ sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no tar-package.tgz "$REMO
 echo "Sleep 1 min to ensure scp package transfer completes..."; \
 sleep 1m
 
-# Connect to remote server via ssh, then...
-# 1. remove existing folder
-# 2. unpackage
-# 3. mv folder
-# 4. cleanup
-
+echo "Connect to remote server via ssh..."
 sshpass -p "$REMOTE_PASS" ssh "$REMOTE_USER"@"$REMOTE_IP" "\
 cd $REMOTE_PATH; \
+
+echo "Remove old directory and unpackage tar..."
 rm -rf creative-developments; \
 tar -xzf tar-package.tgz; \
 mv dist/creative-developments creative-developments/; \
-cd creative-developments/backend; \
+
+echo "Cleanup..."
+rm -rf dist/; \
+rm tar-package.tgz;"
 
 # kill node to bypass `EADDRINUSE, Address/port already in use` error
 killall -9 node
 
 echo "Installing backend dependencies..."
+cd creative-developments/backend; \
 npm i; \
 
-echo "Cleanup..."
-cd ../..; \
-rm -rf dist/; \
-rm tar-package.tgz;"
-
 echo "Start node server..."
-cd creative-developments/backend; \
 npm run start; \
 
 echo "Exit"; \
