55c75d251eb7d1f6b698739730fa0bdfb1a68f18
Omar Mirza <omar.creative.dev@gmail.com>
2021-12-10 09:04:52 +0000

    tweak deploy script

--- travis-deploy-script.sh
+++ travis-deploy-script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
-printf 'Deploy stage started...' >&2
+echo "Deploy stage started..."
 
 # Create zip package
+echo "Create zip package..."
 tar -czf tar-package.tgz dist
 
 # Transfer zip package to remote server via scp
+echo "Transfer zip package to remote server via scp..."
 sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no tar-package.tgz "$REMOTE_USER"@"$REMOTE_IP:$REMOTE_PATH"
 
-# sleep 1 min to ensure scp package transfer completes
+# Sleep 1 min to ensure scp package transfer completes
+echo "Sleep 1 min to ensure scp package transfer completes..."
 sleep 1m
 
 # Connect to remote server via ssh, then...
@@ -26,13 +29,15 @@ cd creative-developments/backend; \
 # kill node to bypass `EADDRINUSE, Address/port already in use` error
 killall -9 node
 
-npm i; \
-echo "Installing backend dependencies..."; \
-npm run start; \
-echo "Server running, now cleanup..."; \
-cd ../..; \
-rm -rf dist/; \
-rm tar-package.tgz;"
+echo "Installing backend dependencies..."
+npm i
+
+echo "Start node server... then cleanup"
+npm run start \
+&& cd ../.. \
+&& rm -rf dist/ \
+&& rm tar-package.tgz
+
 echo "Exit"
 exit 0
 
