f7a2bdb705d47aa7437f2a9e8086f6deef4f00d2
Omar Mirza <omar.creative.dev@gmail.com>
2021-12-10 09:12:56 +0000

    tweak deploy script

--- travis-deploy-script.sh
+++ travis-deploy-script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
-echo "Deploy stage started..."
+echo "Deploy stage started..."; \
 
 # Create zip package
-echo "Create zip package..."
+echo "Create zip package..."; \
 tar -czf tar-package.tgz dist
 
 # Transfer zip package to remote server via scp
-echo "Transfer zip package to remote server via scp..."
+echo "Transfer zip package to remote server via scp..."; \
 sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no tar-package.tgz "$REMOTE_USER"@"$REMOTE_IP:$REMOTE_PATH"
 
 # Sleep 1 min to ensure scp package transfer completes
-echo "Sleep 1 min to ensure scp package transfer completes..."
+echo "Sleep 1 min to ensure scp package transfer completes..."; \
 sleep 1m
 
 # Connect to remote server via ssh, then...
@@ -33,12 +33,12 @@ echo "Installing backend dependencies..."
 npm i
 
 echo "Start node server... then cleanup"
-npm run start \
-&& cd ../.. \
-&& rm -rf dist/ \
-&& rm tar-package.tgz
+npm run start &; \
+cd ../..; \
+rm -rf dist/; \
+rm tar-package.tgz; \
 
-echo "Exit"
+echo "Exit"; \
 exit 0
 
 # Alternatively connect to remote server via ssh, then run deploy script on hosting server
